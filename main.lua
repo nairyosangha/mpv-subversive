ee0595255eaaac6ffdca3c1da8e211dd0d900db7
nairyosangha <nairyo@cock.li>
2024-05-02 19:47:53 +0200

    retab main

--- main.lua
+++ main.lua
@@ -3,21 +3,21 @@ local options = require 'mp.options'
 local loader = require('subloader')
 
 OPTS = {
-	enabled = true,
+    enabled = true,
     cache_directory = "/tmp/subloader",
     subtitle_backend = 'jimaku', -- can be either 'jimaku' or 'offline'
     -- the following options are used when using the 'offline' backend
-	subtitle_mapping = string.format("%s/mapping.csv", mp.get_script_directory()),
+    subtitle_mapping = string.format("%s/mapping.csv", mp.get_script_directory()),
     -- the following options are used when using the 'jimaku' backend
-	API_TOKEN = "",
+    API_TOKEN = "",
 }
 options.read_options(OPTS, 'sub_loader')
 
 local function main()
     local backend = require("backend.backend"):new(OPTS)
-	mp.add_key_binding("b", "find_sub", function() loader:run(backend) end)
+    mp.add_key_binding("b", "find_sub", function() loader:run(backend) end)
 end
 
 if OPTS.enabled then
-	mp.register_event("file-loaded", main)
+    mp.register_event("file-loaded", main)
 end
